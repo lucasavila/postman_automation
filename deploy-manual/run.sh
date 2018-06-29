e2e3d8af16606ca4178f324698d1859a06ef760e
Lucas Avila <lucas.avila.2010@gmail.com>
2018-06-29 13:56:00 -0300

    se agrega run

--- deploy-manual/run.sh
+++ deploy-manual/run.sh
@@ -5,4 +5,4 @@ docker rm postman_automation
 
 echo -n | docker login -u colondocker -p Pnflqfsf01! registry.dev.redbee.io
 
-docker run --name postman_automation --entrypoint=/entrypoint.sh -d  -p 8098:3000 registry.dev.redbee.io/postman_automation:latest
+docker run --name postman_automation --entrypoint=/entrypoint.sh  -p 8098:3000 registry.dev.redbee.io/postman_automation:latest
