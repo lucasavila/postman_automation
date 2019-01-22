2129f12f32e4163758325467be396bcf329816c8
Lucas Avila <avila.lucas@colonseguros.com.ar>
2019-01-22 18:05:10 -0300

    se cambian scirpts

--- deploy-manual/run.sh
+++ deploy-manual/run.sh
@@ -3,6 +3,6 @@
 docker stop postman_automation
 docker rm postman_automation
 
-yes | docker login -u deploycolon -p "#saasdockerhub26566" saascolon
+yes | docker login -u deploycolon -p "#saasdockerhub26566"
 
 docker run --name postman_automation --entrypoint=/entrypoint.sh  -p 8098:3000 saascolon/postman_automation:latest
