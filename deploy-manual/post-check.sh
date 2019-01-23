31586c2cac395153de88752ce869e3934f8aa36b
Lucas Avila <avila.lucas@colonseguros.com.ar>
2019-01-23 09:18:28 -0300

    se cambian scirpts

--- deploy-manual/post-check.sh
+++ deploy-manual/post-check.sh
@@ -2,7 +2,7 @@
 
 POST_CHECK=$(docker logs --tail 30 postman_automation | grep 'failure' | wc -l )
 
-if [ $POST_CHECK -gt 1 ]; then
+if [ $POST_CHECK -gt 0 ]; then
 	echo "LA EJECUCIÓN DE LOS TEST DE AUTOMATION TUVO ERRORES"
 	exit 1
 fi
